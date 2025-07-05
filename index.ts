56f1d65958c3f43a5dd62a2576fe6efe0931421c
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-07-05 17:14:35 +0700

    install logger

--- index.ts
+++ index.ts
@@ -1,9 +1,19 @@
 import 'dotenv/config';
 import express from 'express';
+import morgan from 'morgan';
 
 const port = process.env.PORT;
 
 const server = express();
+
+server.use(morgan('tiny'));
+
+server.post('/login', (req: express.Request, res: express.Response) => {
+  const accessToken = 'vaano';
+  res.json({
+    accessToken,
+  });
+});
 
 server.listen(port, () => {
   console.log(`server running on ${port}`);
