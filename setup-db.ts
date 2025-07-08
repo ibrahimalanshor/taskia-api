3fe6ef6c40dbe332915d0ebcb967bde08e8f3a7f
ibrahimalanshor <ibrahimalanshor6@gmail.com>
2025-07-08 18:47:35 +0700

    task table

--- setup-db.ts
+++ setup-db.ts
@@ -1,8 +1,11 @@
-import Database from 'better-sqlite3';
-
-const db = new Database('taskia.db');
+import { db } from './lib/db';
 
 db.exec('DROP TABLE IF EXISTS users');
 db.exec(
   'CREATE TABLE users (id INTEGER PRIMARY KEY AUTOINCREMENT, email TEXT NOT NULL UNIQUE, name TEXT NOT NULL, google_id TEXT UNIQUE)',
+);
+
+db.exec('DROP TABLE IF EXISTS tasks');
+db.exec(
+  'CREATE TABLE tasks (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT NOT NULL, due_date TEXT NOT NULL, status TEXT NOT NULL)',
 );
